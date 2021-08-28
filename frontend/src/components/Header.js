7d03d8b06c76eb6eed5c2fb2cb9b606cafa6e053
ABHIJITBURMAN <abhijitburman7@gmail.com>
2021-08-28 19:14:47 +0530

    Finial Deployment

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = ({ history }) => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Store</Navbar.Brand>
+            <Navbar.Brand> JGEC Store</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
